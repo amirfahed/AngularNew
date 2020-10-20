5f43f0a71990719b3c1beaf6cdfa237c8a2db170
amirfahed777@gmail.com <amirfahed777@gmail.com>
2020-10-20 18:20:53 +0300

    suzd-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   template: `
    <div>
      <h1>{{pageTitle}}</h1>
-     <div>My First Component </div>
-   </div>
+     <pm-products></pm-products>
+    </div>
 `
 })
 export class AppComponent {
